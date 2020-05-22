634edf78e2f04c5b06937e4166128b5a28c17669
Bandit <mklock75@gmail.com>
2020-05-21 21:38:31 -0700

    Initial Upload

---
+++ SWGSourceLauncher/Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SWGSourceLauncher
+{
+    class Launcher
+    {
+        public static void PlayGame()
+        {
+            Process.Start("SwgClient_r.exe");
+        }
+
+        public static void LaunchWebsite(string url)
+        {
+            Process.Start(url);
+        } 
+    }
+}
