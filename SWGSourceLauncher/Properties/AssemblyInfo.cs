a9279c885b1452f7a1acfe6618b4a987aa6b34d6
Bandit42 <mklock75@gmail.com>
2022-01-08 17:20:58 -0800

    VS updated version

--- SWGSourceLauncher/Properties/AssemblyInfo.cs
+++ SWGSourceLauncher/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SWGSourceLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
